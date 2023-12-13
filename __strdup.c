c68fb9d4ce851b78b7b8ea4fc526180c942c473c
Mohamed Mouyahada <mouyahada@gmail.com>
2023-12-13 05:08:10 +0100

    __strdup.c

---
+++ __strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicate a string
+ *
+ * @str: string
+ *
+ * Return: new str_cpy of @str
+ */
+
+char *_strdup(const char *str)
+{
+	int i;
+	char *str_cpy = malloc(sizeof(char) * (_strlen(str) + 1));
+
+	if (str_cpy == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i])
+	{
+		str_cpy[i] = str[i];
+		i++;
+	}
+	str_cpy[i] = '\0';
+
+	return (str_cpy);
+}
