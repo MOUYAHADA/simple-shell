4ccc7015c15ddb57293ff9e9f5f7d336dc14c71c
Mohamed Mouyahada <mouyahada@gmail.com>
2023-12-13 05:10:03 +0100

    _main.c

---
+++ _main.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - simple shell clone main function
+ *
+ * @ac: argument count
+ * @av: argument vector
+ * @env: environment variables array
+ *
+ * Return: 0 on success
+ */
+
+int main(int ac, char **av, char **env)
+{
+	char *input = NULL;
+	int status = 0, code, count = 0;
+
+	(void)ac;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			_write(STDOUT_FILENO, "$ ");
+		input = _getline();
+		if (input == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				_write(STDOUT_FILENO, "\n");
+			return (status);
+		}
+		count++;
+		code = _check_input(input, av, &status, env, count);
+		if (code == -1)
+			continue;
+		free(input);
+	}
+	return (status);
+}
